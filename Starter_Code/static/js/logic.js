bd597cce42078bc6c16b2345b489f26488251aa5
elluis1001 <elluis.lr1@gmail.com>
2023-12-18 11:20:33 -0600

    Day 2 of coding

--- Starter_Code/static/js/logic.js
+++ Starter_Code/static/js/logic.js
@@ -46,22 +46,24 @@ function createEarthquakeMarkers(data) {
   
     // Loop through the earthquake data and create markers
     data.features.forEach(feature => {
-      const magnitude = feature.properties.mag;
-      const depth = feature.geometry.coordinates[2];
-      const lat = feature.geometry.coordinates[1];
-      const lng = feature.geometry.coordinates[0];
+      let magnitude = feature.properties.mag;
+      let depth = feature.geometry.coordinates[2];
+      let lat = feature.geometry.coordinates[1];
+      let lng = feature.geometry.coordinates[0];
   
       // Create a circle marker for each earthquake
-      const marker = L.circleMarker([lat, lng], {
+      let marker = L.circleMarker([lat, lng], {
         radius: getRadius(magnitude),
         fillColor: getColor(depth),
         color: '#000',
         weight: 1,
         opacity: 1,
         fillOpacity: 0.8
-      }).bindPopup(`<b>Location:</b> ${feature.properties.place}<br><b>Magnitude:</b> ${magnitude}<br><b>Depth:</b> ${depth} km`);
+      // Popups that provide additional information about the earthquake when its associated marker is clicked:
+      }).bindPopup(`<b>Location:</b> ${
+        feature.properties.place}<br><b>Magnitude:</b>, ${magnitude}<br><b>Depth:</b> ${depth} km`);
       
-      // Add the marker to the markers array
+      // Add the marker to the markers array:
       markers.push(marker); 
     });
     
@@ -78,4 +80,41 @@ function createEarthquakeMarkers(data) {
       // Add the markers to a Leaflet layer group and then add to the map
       L.layerGroup(earthquakeMarkers).addTo(myMap);
     });
-    
+
+// Legend:
+function createLegend() {
+  let legend = L.control({position: 'bottomright'});
+
+  //Add legend to map:
+  legend.onAdd = function(map) {
+    let div = L.DomUtil.create('div', 'info legend');
+    let depths = [-10, 20, 50, 100, 300];
+    let labels = [];
+  
+    // Title/heading of the legend:
+    div.innerHTML += '<h4>Earthquake Depth in km.</h4>';
+
+    // Create the label with a colored square:
+    //  Done by looping through the depth intervals:
+    for (let i = 0; i < depths.length; i++) {
+      div.innerHTML +=
+        '<i style="background:' + getColor(depths[i] + 1) + '"></i> ' +
+        depths[i] + (depths[i + 1] ? '&ndash;' + depths[i + 1] + ' km<br>' : '+ km');
+    }
+
+    return div;
+
+  };
+  
+    // d3.json to get data from URL:
+    d3.json(url).then(function (data) {
+      // Create earthquake markers:
+      let legendQuake = createLegend(); // Remove the "data" argument
+
+      // Add the legend to the map
+      legendQuake.addTo(myMap);
+    });
+  
+  legend.addTo(map);
+  
+ };
